1e50316898dcd585af0a333a0b672aebf5d9dc49
Ashwini Dubey <ak213414@gmail.com>
2026-06-11 22:30:26 +0530

    Arrow Functions

---
+++ L20_Function_Arrow.js
@@ -0,0 +1,18 @@
+/*
+Arrow Function:
+Arrow functions provide a shorter syntax for writing functions. They do not have their own 'this' context and are always anonymous.
+
+Syntax:
+
+const functionName = () => {
+    // code
+};
+  
+*/
+
+const printName = () => {
+    console.log("This is an arrow function.");
+}
+
+printName();
+
