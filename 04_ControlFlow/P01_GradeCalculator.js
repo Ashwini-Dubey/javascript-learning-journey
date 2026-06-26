25c4f887a7b0d64d57737be97a5a81ef44f6a44e
Ashwini Dubey <ak213414@gmail.com>
2026-06-26 23:59:36 +0530

    PracticeExercise_ControlFlow

---
+++ 04_ControlFlow/P01_GradeCalculator.js
@@ -0,0 +1,14 @@
+const studentGrade = 40;
+
+if (studentGrade >= 90) {
+    console.log("You got an A!");
+} 
+else if (studentGrade >= 75 && studentGrade < 90) {
+    console.log("You got a B!");
+} 
+else if (studentGrade >= 60 && studentGrade < 75) {
+    console.log("You got a C!");
+}  
+else {
+    console.log("You got an F!");
+}
