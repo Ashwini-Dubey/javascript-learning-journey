b04a2097f3b03c24fc3dc22f0273a7133e572261
Ashwini Dubey <ak213414@gmail.com>
2026-07-10 22:33:34 +0530

    Changes to the Arrays

--- 07_DataStructures/L23_DataStructures_Arrays.js
+++ 07_DataStructures/L23_DataStructures_Arrays.js
@@ -27,3 +27,26 @@ console.log(colors.at(-2));
 console.log(colors.length); // Output: 5
 console.log(colors.includes("Yellow"));
 console.log(colors.indexOf("Green"));
+
+/*
+Array.map() is a method that creates a new array by applying a provided function to each element of the original array. It does not modify the original array but instead returns a new array with the transformed values.
+
+Syntax:
+const newArray = originalArray.map(callback(currentValue, index, array) {
+  // Return the new value for the new array
+});
+
+Note:
+
+1. A method available on arrays.
+2. Used to transform array elements
+3. Returns a new array with the transformed values
+4. Doesn't modify the original array (unless you do it yourself)
+*/
+
+const price = [5000, 10000, 15000, 20000, 25000];
+
+const discountedPrice = price.map(value => value * 0.5)
+
+console.log(price);
+console.log(discountedPrice);
--- 07_DataStructures/L24_DataStructures_Push_Pop_Mutations.js
+++ 07_DataStructures/L24_DataStructures_Push_Pop_Mutations.js
@@ -22,52 +22,52 @@ const stack = [];
 // push adds to the end of the array
 stack.push("first");
 stack.push("second","third");
-console.log(stack);
+console.log("Example of push method:", stack);
 
 // pop removes the last item from the array and returns it
 const last = stack.pop();
 
 // the stack is mutated, the last item is removed
-console.log(last);
-console.log(stack);
+console.log("Example of pop method - Removed Element:", last);
+console.log("Example of pop method:", stack);
 
 // shift removes the first item from the array and returns it
 const first = stack.shift();
 
 // the stack is mutated, the first item is removed
-console.log(first);
-console.log(stack);
+console.log("Example of shift method - Removed Element:", first);
+console.log("Example of shift method:", stack);
 
 // unshift adds items to the beginning of the array
 stack.unshift("new first");
-console.log(stack); 
+console.log("Example of unshift method:", stack); 
 
 //splice can be used to add or remove items at any position in the array
 // to add items, we specify the index to start at, the number of items to remove (0 in this case), and the items to add.
 stack.splice(1, 0, "new second"); // adds "new second" at index 1 without removing any items
-console.log(stack);
+console.log("Example of splice method:", stack);
 
 stack.splice(2, 1); // removes 1 item at index 2
-console.log(stack); 
+console.log("Example of splice method:", stack);
 
 // sort can be used to sort the items in the array
 stack.push("another item");
 stack.push("yet another item");
-console.log(stack);
+console.log( stack);
 stack.sort(); // sorts the items in place
-console.log(stack); 
+console.log("Example of sort method:", stack); 
 
 // reverse can be used to reverse the order of the items in the array
 stack.reverse(); // reverses the items in place
-console.log(stack); 
+console.log("Example of reverse method:", stack); 
 
 // fill can be used to fill the array with a specific value
 stack.fill("filled"); // fills the entire array with "filled"
-console.log(stack); 
+console.log("Example of fill method:",stack); 
 
 // splice can also be used to replace items in the array
 stack.splice(1, 2, "replaced"); // replaces 2 items at index 1 with "replaced"
-console.log(stack);
+console.log("Example of splice method:", stack);
 
 // copyWithin can be used to copy a portion of the array to another location within the same array
 stack.push("copy this");
@@ -75,4 +75,4 @@ stack.push("and this");
 console.log(stack);
 
 stack.copyWithin(0, 2, 4); // copies items from index 2 to 4 and pastes them starting at index 0
-console.log(stack); 
+console.log("Example of copyWithin method:", stack); 
